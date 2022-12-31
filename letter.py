0b370de745f1fe9a74ba4eba95dd1a21be0fd395
Yousef Altaher <yousef.a.altaher@gmail.com>
2022-12-31 16:00:58 +0000

    Test creating letters with quadratic substrucures

--- letter.py
+++ letter.py
@@ -1,39 +1,17 @@
 
-class Stroke:
+class AtomicStroke:
     
-    def __init__(self, coeff):
+    def __init__(self, coeff = None, reflector = None):
         self.slopeQuadraticCoeff = coeff
+        self.reflector = reflector
+
+class Stroke:
+
+    def __init__(self, name, atomicStrokes = None):
+        self.name = name
+        self.atomicStrokes = atomicStrokes
 
 class Letter:
-
-    def __init__(self, name):
+    def __init__(self, name, strokes):
         self.name = name
-        self.strokesBefore = []
-        self.strokesMiddle = []
-        self.strokesAfter = []
-
-    def getName(self):
-        return self.name
-
-    def setStrokes(self, strokes, pos):
-        if pos == "before": self.setStrokesBefore(strokes)
-        if pos == "middle": self.setStrokesBefore(strokes)
-        if pos == "after": self.setStrokesBefore(strokes)
-
-    def setStrokesBefore(self, strokes):
-        self.strokesBefore = strokes
-    
-    def setStrokesMiddle(self, strokes):
-        self.strokesMiddle = strokes
-
-    def setStrokesAfter(self, strokes):
-        self.strokesAfter = strokes
-
-    def getStrokesBefore(self):
-        return self.strokesBefore
-    
-    def getStrokesMiddle(self):
-        return self.strokesMiddle
-
-    def getStrokesAfter(self):
-        return self.strokesAfter
+        self.strokes = strokes
--- main.py
+++ main.py
@@ -1,40 +1,95 @@
-from letter import Stroke, Letter
+from letter import AtomicStroke, Stroke, Letter
 from json import load
 from random import uniform
-#import svgwrite
+import svgwrite
+import matplotlib.pyplot as plt
 
 alphabet = {}
+strokes = {}
+
+
+def seedAtomicStrokes():
+    global strokes
+
+    data = load(open('data/atomicStrokes.json'))
+    for i in data:
+        stroke = Stroke(i['atomicStroke'])
+
+        lst = []
+        for s in i['strokes']:
+            lst.append(AtomicStroke(((s['a'][0], s['a'][1]), (s['b'][0], s['b'][1]), (s['c'][0], s['c'][1])), s['reflector']))
+
+        stroke.atomicStrokes = lst
+        strokes[stroke.name] = stroke
+
+
+def seedLetters():
+    global alphabet
+
+    data = load(open('data/letters.json'))
+    for i in data:
+        alphabet[i['name']] = Letter(i['name'], i["atomicStrokes"])
+
 
 def seed():
+    seedAtomicStrokes()
+    seedLetters()
+
+
+def generateSkeleton(letter):
     global alphabet
+    global strokes
 
-    data = load(open('data.json'))
+    letterStrokes = alphabet[letter].strokes
+
+    data = []
+    for i in letterStrokes:
+        for j in strokes[i].atomicStrokes:
+            data.append((uniform(j.slopeQuadraticCoeff[0][0], j.slopeQuadraticCoeff[0][1]), uniform(j.slopeQuadraticCoeff[1][0], j.slopeQuadraticCoeff[1][1]), uniform(j.slopeQuadraticCoeff[2][0], j.slopeQuadraticCoeff[2][1])))
+
+    return data
+
+
+def generateLetter(letter):
+    return generateSkeleton(letter)
+
+
+def sample(data, sampleRate = 0.01):
+    lst = []
+    i = 0
+    while i < 1:
+        lst.append((i, (data[0]*i*i+data[1]*i+data[2])))
+        i += sampleRate
+
+    return lst
+
+def draw(data):
+    x = []
+    y = []
+    aa = 0
     for i in data:
-        letter = Letter(i['letter'])
+        points = sample(i)
+        m = 0
+        if (len(y) != 0):
+            m = y[-1]
+        
+        x += list(map(lambda a: a[0] - aa, points))
+        y += list(map(lambda a: a[1], points))
+        aa += 1
+        print("------------------")
+        print(x)
+        print(y)
 
-        for p in i['strokes']:
-            lst = []
-            for s in i['strokes'][p]:
-                lst.append(Stroke(((s['a'][0], s['a'][1]), (s['b'][0], s['b'][1]), (s['c'][0], s['c'][1]))))
-
-            letter.setStrokes(lst, p)
-
-        alphabet[letter.getName()] = letter
-
+    plt.plot(x, y)
+    plt.savefig(f"a.png")
+        
+        # polyLine = svgwrite.shapes.Polyline()
+        # polyLine.points = points
+        # polyLine.translate(100, 100)
+        # # reflect along 100, 100
+        # dwg = svgwrite.Drawing(f'{i}.svg')
+        # dwg.add(polyLine)
+        # dwg.save()
 
 seed()
-
-def drawLetter(letter):
-    global alphabet
-    beforeS = alphabet[letter].getStrokesBefore()[0].slopeQuadraticCoeff
-
-    print(uniform(beforeS[0][0], beforeS[0][1]))
-    print(uniform(beforeS[1][0], beforeS[1][1]))
-    print(uniform(beforeS[2][0], beforeS[2][1]))
-
-    beforeS2 = alphabet[letter].getStrokesBefore()[1].slopeQuadraticCoeff
-    print(uniform(beforeS2[0][0], beforeS2[0][1]))
-    print(uniform(beforeS2[1][0], beforeS2[1][1]))
-    print(uniform(beforeS2[2][0], beforeS2[2][1]))
-
-drawLetter('b')
+draw(generateLetter('A'))
